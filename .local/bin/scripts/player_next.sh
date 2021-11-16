12e2c11b6521e70b63c6d3b14b03ff9fd365bc70
nixfu <nixfu.nixfu@gmail.com>
2021-11-16 12:23:44 -0500

    Add checks for playerctl being installed

--- .local/bin/scripts/player_next.sh
+++ .local/bin/scripts/player_next.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# make sure playerctl is installed
+if ! command -v playerctl &> /dev/null
+then
+        exit 0
+fi
+
 # if you want to filter out browsers
 for player in $(playerctl -l |grep -v -e firefox -e chromium)
 do 
